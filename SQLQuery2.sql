e201ecac13a5295af12ea09025ddab5a90fd9fd9
ChirazMansour <82649102+ChirazMansour@users.noreply.github.com>
2021-04-23 11:03:46 +0100

    Update SQLQuery2.sql

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -1,19 +1,15 @@
 use AdventureWorks2017
 go
 
-select * from Sales.SalesOrderHeader;
-
 select [SalesOrderID],[OrderDate],[TotalDue]
 from Sales.SalesOrderHeader
 where OrderDate BETWEEN '2011-09-01' AND'2011-09-30' AND TotalDue > 3500;
 
-select * from Person.Person
 
 select [BusinessEntityID],[FirstName],[MiddleName],[LastName]
 from Person.Person
 order by LastName;
 
-select * from Sales.SpecialOffer
 
 select [SpecialOfferID],MaxQty - MinQty,[Description]
 from Sales.SpecialOffer;
@@ -22,5 +18,11 @@ select [SalesOrderID],[OrderDate],[ShipDate],DATEDIFF(d,OrderDate,ShipDate) AS N
 from Sales.SalesOrderHeader
 
 
-select [CustomerID],[FirstName],[LastName]
-from Sales.Customer INNER JOIN Person.Person on ;
+select FirstName, MiddleName, LastName, Prod.Name
+from Sales.Customer AS C
+INNER JOIN Person.Person AS P ON C.PersonID = P.BusinessEntityID
+INNER JOIN Sales.SalesOrderHeader AS SOH ON C.CustomerID = SOH.CustomerID
+INNER JOIN Sales.SalesOrderDetail AS SOD
+ON SOH.SalesOrderID = SOD.SalesOrderID
+INNER JOIN Production.Product AS Prod ON SOD.ProductID = Prod.ProductID;
+
